3b1b6df77b35d7f9d8fc480bbf7cf9837e3d9a89
Boom <93481972+lnwboom@users.noreply.github.com>
2024-06-02 17:51:57 +0700

    edit home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useContext } from 'react';
+import { UserContext } from "../../context/userContext"
 
 export default function Home() {
+  const { user } = React.useContext(UserContext);
 
   return (
     <div>
       <h1>Welcome !!</h1>
+      {!!user && <h2>Hi {user.name} !</h2>}
     </div>
   );
 }
